aad07346348a10eaa664e5cd28b9ce81d9880844
VitoZMX <Vi-69@yandex.ru>
2023-05-16 20:49:12 +0300

    refactor: components profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {Container, Grid} from '@material-ui/core'
+import {Context} from '../index'
+import {useAuthState} from 'react-firebase-hooks/auth'
 
 export function Profile() {
+    const {auth} = useContext(Context)
+    const [user] = useAuthState(auth)
+
     return (
-        <div>
-            Profile
-        </div>
+        <Container>
+            <Grid
+                container
+                justifyContent="center"
+            >
+                <div style={{marginTop:'150px'}}>
+                    <div>Profile Page</div>
+                    <img  src={user?.photoURL || undefined} alt="User avatar"/>
+                    <div>Name: {user?.displayName}</div>
+                </div>
+            </Grid>
+        </Container>
     )
 }
