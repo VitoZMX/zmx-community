d6198ba20a0ce29e5880005ac0ce0c2b0118e7bc
VitoZMX <Vi-69@yandex.ru>
2023-05-20 15:24:59 +0300

    feat: add display info on profile page

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,23 +1,89 @@
 import React, {useContext} from 'react'
-import {Container, Grid} from '@material-ui/core'
+import {Container} from '@material-ui/core'
+import Grid from '@mui/material/Grid'
 import {Context} from '../index'
 import {useAuthState} from 'react-firebase-hooks/auth'
+import Avatar from '@mui/material/Avatar'
+import Typography from '@mui/material/Typography'
 
 export function Profile() {
     const {auth} = useContext(Context)
     const [user] = useAuthState(auth)
 
     return (
-        <Container>
-            <Grid
-                container
-                justifyContent="center"
-            >
-                <div style={{marginTop:'150px'}}>
-                    <div>Profile Page</div>
-                    <img  src={user?.photoURL || undefined} alt="User avatar"/>
-                    <div>Name: {user?.displayName}</div>
-                </div>
+        <Container style={{marginTop: '80px'}}>
+            <Grid container spacing={1} alignItems="stretch">
+                <Grid item xs={4}>
+                    <div style={{
+                        backgroundColor: 'rgba(25,118,210,0.2)',
+                        boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.15)',
+                        borderRadius: '12px',
+                        padding: '15px',
+                        height: '100%'
+                    }}>
+                        <Avatar alt={user?.displayName || 'ProfileAvatar'}
+                                src={user?.photoURL || undefined}
+                                sx={{width: 200, height: 200, boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.5)'}}
+                        />
+                        <Typography variant="h5" sx={{
+                            mr: 2,
+                            fontFamily: 'Roboto',
+                            fontWeight: 100,
+                            color: 'black'
+                        }}>{user?.email}</Typography>
+                        <h4>Тут должна быть информация о тебе брат!Тут должна быть информация о тебе брат!Тут должна
+                            быть
+                            информация о тебе брат!Тут должна быть информация о тебе брат!Тут должна быть информация о
+                            тебе
+                            брат!</h4>
+                        <Typography variant="subtitle2" sx={{
+                            mr: 2,
+                            fontFamily: 'Roboto',
+                            fontWeight: 100,
+                            color: 'white'
+                        }}>Account created: {user?.metadata.creationTime}</Typography>
+                        <Typography variant="subtitle2" sx={{
+                            mr: 2,
+                            fontFamily: 'Roboto',
+                            fontWeight: 100,
+                            color: 'white'
+                        }}>ID: {user?.uid}</Typography>
+                    </div>
+                </Grid>
+                <Grid item xs={8}>
+                    <div style={{
+                        backgroundColor: 'rgba(25,118,210,0.2)',
+                        boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.15)',
+                        borderRadius: '12px',
+                        padding: '15px',
+                        height: '100%'
+                    }}>
+                        <Typography variant="h3" sx={{
+                            mr: 2,
+                            textShadow: '2px 2px 4px rgba(0,0,0,0.2)',
+                            fontFamily: 'Roboto',
+                            fontWeight: 350,
+                            color: 'black'
+                        }}>{user?.displayName}</Typography>
+                        <h4>И тут тоже нужно что то написать о тебе, братишка! И тут тоже нужно что то написать о тебе,
+                            братишка!И тут тоже нужно что то написать о тебе, братишка!И тут тоже нужно что то написать
+                            о
+                            тебе, братишка!</h4>
+                    </div>
+                </Grid>
+                <Grid item xs={12}>
+                    <div style={{
+                        backgroundColor: 'rgba(25,118,210,0.2)',
+                        boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.15)',
+                        borderRadius: '12px',
+                        padding: '15px'
+                    }}>
+                        <h4>Блок для добавление новости. xs=12 Блок для добавление новости. xs=12Блок для добавление
+                            новости. xs=12Блок для добавление новости. xs=12Блок для добавление новости. xs=12Блок для
+                            добавление новости. xs=12Блок для добавление новости. xs=12Блок для добавление новости.
+                            xs=12Блок для добавление новости. xs=12Блок для добавление новости. xs=12</h4>
+                    </div>
+                </Grid>
             </Grid>
         </Container>
     )
