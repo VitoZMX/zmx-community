0ba244652e68239f8d055c8bedbf72d0103ba96f
VitoZMX <Vi-69@yandex.ru>
2023-06-05 12:54:33 +0300

    feat: add link to NewsMore page

--- src/components/CartNews.tsx
+++ src/components/CartNews.tsx
@@ -6,37 +6,41 @@ import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
 import {CardActionArea, useTheme} from '@mui/material'
+import {NavLink} from 'react-router-dom'
 
 type CartNewsPropsType = {
     img: string,
     title: string,
-    text: string
+    text: string,
+    id: string
 }
 
-export function CartNews({img, title, text}: CartNewsPropsType) {
+export function CartNews({img, title, text, id}: CartNewsPropsType) {
     const {auth} = useContext(Context)
     const [user] = useAuthState(auth)
     const theme = useTheme()
 
     return (
+
         <Card sx={{
             maxWidth: theme.breakpoints.down('sm') ? 'auto' : '345',
             display: 'flex',
             alignItems: 'flex-start',
             height: '100%'
         }}>
-            <CardActionArea>
+
+            <CardActionArea component={NavLink} to={`/news/${id}`} sx={{height: '100%'}}>
                 <CardMedia
+                    sx={{objectFit: 'cover', height: '140px'}}
                     component="img"
-                    height="140"
                     image={img}
                     alt="alt"
                 />
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
+                    <Typography style={{textIndent: '12px'}} gutterBottom variant="h5" component="div">
                         {title}
                     </Typography>
-                    <Typography variant="body2" color="text.secondary">
+                    <Typography style={{textIndent: '6px'}} variant="body2" color="text.secondary">
                         {text}
                     </Typography>
                 </CardContent>
