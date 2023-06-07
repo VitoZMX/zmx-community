20899ecccb5b1a2593e7e051ea0000960e42bf2c
VitoZMX <Vi-69@yandex.ru>
2023-06-07 18:13:07 +0300

    feat: implement replace photo profile

--- src/components/SettingPage.tsx
+++ src/components/SettingPage.tsx
@@ -1,7 +1,6 @@
-import React, {useContext} from 'react'
+import React, {ChangeEvent, useContext, useEffect, useState} from 'react'
 import {Container} from '@material-ui/core'
 import Grid from '@mui/material/Grid'
-import {Context} from '../index'
 import {useAuthState} from 'react-firebase-hooks/auth'
 import Typography from '@mui/material/Typography'
 import ImageSearchIcon from '@mui/icons-material/ImageSearch'
@@ -10,15 +9,118 @@ import CardMedia from '@mui/material/CardMedia'
 import EditIcon from '@mui/icons-material/Edit'
 import imgF from '../assets/image/logoNoImg.png'
 import Switch from '@mui/material/Switch'
+import {deleteObject, getDownloadURL, getStorage, ref, uploadBytes} from 'firebase/storage'
+import {collection, getFirestore} from 'firebase/firestore'
+import {updateProfile, User as FirebaseUser} from 'firebase/auth'
+import {Context} from '../App'
 
 export function SettingPage() {
     const {auth} = useContext(Context)
     const [user] = useAuthState(auth)
+    const [profile, setUser] = useState<FirebaseUser | null>(null)
     const [checked, setChecked] = React.useState(false)
+    const [selectedFile, setSelectedFile] = useState<File | null>(null)
+    const firestore = getFirestore()
+    const inputRef = React.useRef<HTMLInputElement>()
+    const usersCollection = collection(getFirestore(), 'users')
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setChecked(event.target.checked)
     }
+
+    const PhotoSelected = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files.length) {
+            let file = e.target.files[0]
+            setSelectedFile(file)
+        }
+    }
+
+    useEffect(() => {
+        if (selectedFile) {
+            sendPhotoSelected()
+        }
+    }, [selectedFile])
+
+    const sendPhotoSelected = async () => {
+        if (!user) return
+
+        if (selectedFile) {
+            console.log('start')
+            const storage = getStorage()
+            const storageRef = ref(storage, `usersAvatar/${selectedFile.name}`)
+            const snapshot = await uploadBytes(storageRef, selectedFile)
+            const imgUrl = await getDownloadURL(snapshot.ref)
+
+            if (user.photoURL) {
+                const storageRefOldAvatar = ref(storage, user?.photoURL)
+
+                deleteObject(storageRefOldAvatar)
+                    .then(() => {
+                        console.log('File deleted successfully')
+                    })
+                    .catch((error) => {
+                        console.error('Error deleting file:', error)
+                    })
+            }
+
+            // отправка ссылок на аватарки в новую коллекцию
+            /*const userDocRef = doc(firestore, 'users', user.uid)
+            await updateDoc(userDocRef, newsData)*/
+
+            if (auth.currentUser) {
+                await updateProfile(auth.currentUser, {
+                    photoURL: imgUrl
+                })
+                const updatedUser = {
+                    ...auth.currentUser,
+                    photoURL: imgUrl
+                }
+                setUser(updatedUser)
+                console.log('Profile updated!')
+            } else {
+                setUser(null)
+            }
+        }
+    }
+
+    /*const sendProfileDataHandler = async () => {
+        if (!user) return
+
+        /!*if (!valueTitle.trim()) {
+            ClearMessageHandle()
+            return
+        }*!/
+
+        if (user) {
+            let imgUrl = ''
+            if (selectedFile) {
+                const storage = getStorage()
+                const storageRef = ref(storage, `ProfilePhoto/${selectedFile.name}`)
+                const snapshot = await uploadBytes(storageRef, selectedFile)
+                imgUrl = await getDownloadURL(snapshot.ref)
+            }
+            const newsData = {
+                title: valueTitle,
+                MinText: valueMinText,
+                MaxText: valueMaxText,
+                userId: user.uid,
+                img: imgUrl,
+                createdAt: serverTimestamp()
+            }
+            try {
+                const newMessageRef = await addDoc(newsCollection, newsData)
+                console.log('Message sent successfully! ID:', newMessageRef.id)
+                setValueTitle('')
+                setValueMinText('')
+                setValueMaxText('')
+                ClearMessageHandle()
+                setSelectedFile(null)
+                handleClose()
+            } catch (error) {
+                console.error('Error sending message: ', error)
+            }
+        }
+    }*/
 
     return (
         <Container style={{marginTop: '80px'}}>
@@ -36,9 +138,9 @@ export function SettingPage() {
                             alt="Your avatar"
                         />
                         <div style={{padding: '10px'}}>
-                            <Button variant="outlined" onClick={() => {
-                            }} style={{marginBottom: '10px', width: '100%'}}>
+                            <Button variant="outlined" component="label" style={{marginBottom: '10px', width: '100%'}}>
                                 <ImageSearchIcon/>
+                                <input type="file" onChange={PhotoSelected} hidden/>
                                 new avatar
                             </Button>
                             <Typography variant="subtitle2" sx={{
