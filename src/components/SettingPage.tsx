179fe0fd931f64637c34a5fc8a34119fc3370e8a
VitoZMX <Vi-69@yandex.ru>
2023-06-08 18:34:53 +0300

    feat: add button effect

--- src/components/SettingPage.tsx
+++ src/components/SettingPage.tsx
@@ -11,27 +11,79 @@ import imgF from '../assets/image/logoNoImg.png'
 import Switch from '@mui/material/Switch'
 import {deleteObject, getDownloadURL, getStorage, ref, uploadBytes} from 'firebase/storage'
 import {collection, getFirestore} from 'firebase/firestore'
-import {updateProfile, User as FirebaseUser} from 'firebase/auth'
+import {updateProfile} from 'firebase/auth'
 import {Context} from '../App'
+import {AlertDialogSlide} from './AlertFullScreen'
+import {green} from '@mui/material/colors'
 
 export function SettingPage() {
+    const [loading, setLoading] = React.useState(false)
+    const [success, setSuccess] = React.useState(false)
+    const timer = React.useRef<number>()
     const {auth} = useContext(Context)
+    const {setUser} = useContext(Context)
     const [user] = useAuthState(auth)
-    const [profile, setUser] = useState<FirebaseUser | null>(null)
     const [checked, setChecked] = React.useState(false)
     const [selectedFile, setSelectedFile] = useState<File | null>(null)
-    const firestore = getFirestore()
+    const [showAlert, setShowAlert] = useState(false)
     const inputRef = React.useRef<HTMLInputElement>()
     const usersCollection = collection(getFirestore(), 'users')
+    const avatarRequirements = 'Format: .jpg, .jpeg, .png, .gif. Max size: 10Mb'
+
+    const buttonSx = {
+        ...(success && {
+            bgcolor: green[500],
+            color: green[100],
+            borderColor: green[200],
+            '&:hover': {
+                bgcolor: green[700],
+                color: green[100],
+                borderColor: green[200],
+            },
+        }),
+    }
+
+    React.useEffect(() => {
+        return () => {
+            clearTimeout(timer.current)
+        }
+    }, [])
+
+    const handleButtonClick = () => {
+        if (!loading) {
+            setSuccess(false)
+        }
+    }
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setChecked(event.target.checked)
     }
 
     const PhotoSelected = (e: ChangeEvent<HTMLInputElement>) => {
+        setLoading(true)
+
         if (e.target.files && e.target.files.length) {
             let file = e.target.files[0]
-            setSelectedFile(file)
+            const maxSize = 10 * 1024 * 1024 // 10 МБ в байтах
+            const allowedTypes = ['image/jpeg', 'image/png', 'image/gif']
+            const allowedExtensions = ['.jpg', '.jpeg', '.png', '.gif']
+
+            if (file.size <= maxSize && allowedTypes.includes(file.type)) {
+                const fileExtension = `.${file.name.split('.').pop()}`
+
+                if (allowedExtensions.includes(fileExtension.toLowerCase())) {
+                    setSelectedFile(file)
+                    console.log('File is valid')
+                } else {
+                    setShowAlert(true)
+                    setLoading(false)
+                    console.error('Invalid file extension')
+                }
+            } else {
+                setShowAlert(true)
+                setLoading(false)
+                console.error('Invalid file size or type')
+            }
         }
     }
 
@@ -47,42 +99,59 @@ export function SettingPage() {
         if (selectedFile) {
             console.log('start')
             const storage = getStorage()
-            const storageRef = ref(storage, `usersAvatar/${selectedFile.name}`)
+            const storageRef = ref(storage, `usersAvatar/${user.uid}___${selectedFile.name}`)
             const snapshot = await uploadBytes(storageRef, selectedFile)
             const imgUrl = await getDownloadURL(snapshot.ref)
 
             if (user.photoURL) {
-                const storageRefOldAvatar = ref(storage, user?.photoURL)
+                let storageRefOldAvatar = null
 
-                deleteObject(storageRefOldAvatar)
-                    .then(() => {
-                        console.log('File deleted successfully')
-                    })
-                    .catch((error) => {
-                        console.error('Error deleting file:', error)
-                    })
-            }
-
-            // отправка ссылок на аватарки в новую коллекцию
-            /*const userDocRef = doc(firestore, 'users', user.uid)
-            await updateDoc(userDocRef, newsData)*/
-
-            if (auth.currentUser) {
-                await updateProfile(auth.currentUser, {
-                    photoURL: imgUrl
-                })
-                const updatedUser = {
-                    ...auth.currentUser,
-                    photoURL: imgUrl
+                try {
+                    storageRefOldAvatar = ref(storage, user.photoURL)
+                    await deleteObject(storageRefOldAvatar)
+                        .then(() => {
+                            console.log('File deleted successfully')
+                            updatePhotoUser(imgUrl)
+                        })
+                        .catch((error) => {
+                            console.error('Error deleting file:', error)
+                            updatePhotoUser(imgUrl)
+                        })
+                } catch (error) {
+                    console.error('Пользователь впервые сменил дефолтную аватарку', error)
+                    updatePhotoUser(imgUrl)
                 }
-                setUser(updatedUser)
-                console.log('Profile updated!')
+                // отправка ссылок на аватарки в новую коллекцию
+                /*const userDocRef = doc(firestore, 'users', user.uid)
+                await updateDoc(userDocRef, newsData)*/
             } else {
-                setUser(null)
+                updatePhotoUser(imgUrl)
             }
         }
+        setSelectedFile(null)
     }
 
+    const updatePhotoUser = async (imgUrl: string | null) => {
+        if (auth.currentUser) {
+            await updateProfile(auth.currentUser, {
+                photoURL: imgUrl,
+            })
+            const updatedUser = {
+                ...auth.currentUser,
+                photoURL: imgUrl,
+            }
+            setUser(updatedUser)
+            setLoading(false)
+            setSuccess(true)
+            timer.current = window.setTimeout(() => {
+                setSuccess(false)
+                setLoading(false)
+            }, 4000)
+            console.log('Profile updated!')
+        } else {
+            setUser(null)
+        }
+    }
     /*const sendProfileDataHandler = async () => {
         if (!user) return
 
@@ -124,8 +193,9 @@ export function SettingPage() {
 
     return (
         <Container style={{marginTop: '80px'}}>
-            <Grid container spacing={1} alignItems="stretch">
-                <Grid item sm={12} md={4}>
+            {showAlert && <AlertDialogSlide text={avatarRequirements} title={'No valid data'}/>}
+            {<Grid container spacing={1} alignItems="stretch">
+                <Grid item sm={12} lg={4} md={4} xs={12}>
                     <div style={{
                         backgroundColor: 'rgba(25,118,210,0.2)',
                         boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.15)',
@@ -138,11 +208,27 @@ export function SettingPage() {
                             alt="Your avatar"
                         />
                         <div style={{padding: '10px'}}>
-                            <Button variant="outlined" component="label" style={{marginBottom: '10px', width: '100%'}}>
+                            <Button variant="outlined" component="label"
+                                    sx={buttonSx}
+                                    disabled={loading}
+                                    onClick={handleButtonClick}
+                                    style={{marginBottom: '2px', width: '100%'}}>
                                 <ImageSearchIcon/>
-                                <input type="file" onChange={PhotoSelected} hidden/>
+                                <input type="file"
+                                       accept=".jpg,.jpeg,.png,.gif"
+                                       name="imgfile"
+                                       max="10485760"
+                                       onChange={PhotoSelected}
+                                       style={{display: 'none'}}
+                                />
                                 new avatar
                             </Button>
+                            <Typography variant="body2" sx={{
+                                mr: 2,
+                                fontFamily: 'Roboto',
+                                fontWeight: 100,
+                                marginBottom: '10px',
+                            }}>{avatarRequirements}</Typography>
                             <Typography variant="subtitle2" sx={{
                                 mr: 2,
                                 fontFamily: 'Roboto',
@@ -158,7 +244,7 @@ export function SettingPage() {
                         </div>
                     </div>
                 </Grid>
-                <Grid item md={8} sm={12}>
+                <Grid item sm={12} lg={8} xs={12} md={8}>
                     <div style={{
                         backgroundColor: 'rgba(25,118,210,0.2)',
                         boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.15)',
@@ -209,7 +295,7 @@ export function SettingPage() {
                         <h4>Тут можно удалить свои посты.</h4>
                     </div>
                 </Grid>
-            </Grid>
+            </Grid>}
         </Container>
     )
 }
